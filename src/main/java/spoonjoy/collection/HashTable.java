ed363ec7bb0f5b34b4d01a9bce3486f9ebd0de8a
nishant <nishant.singh87@gmail.com>
2014-10-10 20:29:03 +0530

    adding hashtable.lessThan

--- src/main/java/spoonjoy/collection/HashTable.java
+++ src/main/java/spoonjoy/collection/HashTable.java
@@ -4,7 +4,7 @@ import static java.lang.Math.abs;
 
 public class HashTable<K extends Comparable, V> {
     private SortedBucket<K, V>[] sortedBuckets;
-    private static final int DEFAULT_TABLE_SIZE = 99999999;
+    private static final int DEFAULT_TABLE_SIZE = 999;
     private static final int INITIAL_BUCKET_SIZE = 2;
 
     public HashTable() {
@@ -28,5 +28,33 @@ public class HashTable<K extends Comparable, V> {
 
     public V get(K key) {
         return bucketFor(key).get(key);
+    }
+
+    public V[] lessThan(K key) {
+        Object[][] values = new Object[sortedBuckets.length][];
+        for (int i = 0; i < sortedBuckets.length; i++) {
+            SortedBucket<K, V> bucket = sortedBuckets[i];
+            if(bucket !=null) values[i] = bucket.valuesSmallerThan(key);
+        }
+        return (V[])flattened(values);
+    }
+
+    public static Object[] flattened(Object[][] values) {
+        int totalElements = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != null && values[i].length != 0){
+                totalElements += values[i].length;
+            }
+        }
+        Object[] result = new Object[totalElements];
+        int elementsAdded = 0;
+        for (Object[] subArr : values) {
+            if (subArr != null && subArr.length !=0) {
+                for (Object value : subArr) {
+                    result[elementsAdded++] = value;
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/spoonjoy/collection/HashTableTest.java
+++ src/test/java/spoonjoy/collection/HashTableTest.java
@@ -31,4 +31,22 @@ public class HashTableTest {
         assertThat(hashTable.get("1"), is("one"));
         assertThat(hashTable.get("2"), is("two"));
     }
+
+    @Test
+    public void shouldGetValueInRange() {
+        hashTable.put("3", "three");
+        hashTable.put("5", "five");
+        hashTable.put("2", "two");
+        hashTable.put("6", "six");
+        hashTable.put("4", "four");
+
+        assertThat(hashTable.lessThan("7"), is(arrayOf("two","three", "four", "five", "six")));
+        assertThat(hashTable.lessThan("5"), is(arrayOf("two","three", "four")));
+        assertThat(hashTable.lessThan("2"), is(arrayOf()));
+        assertThat(hashTable.lessThan("1"), is(arrayOf()));
+    }
+
+    private <T> T[] arrayOf(T...values) {
+        return values;
+    }
 }
